936c694ce74d457b2f87ee4c37109fab307ceef1
Oleksandr Petrov <petrovoleksandr03@gmail.com>
2023-03-23 17:02:37 +0100

    skeleton added

---
+++ Project/ClothesShop/Services/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace Services
+{
+    [Route("api/profile")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost("register-services2")]
+        public async Task<IActionResult> Register1()
+        {
+            return Ok(ModelState);
+
+        }
+    }
+}
